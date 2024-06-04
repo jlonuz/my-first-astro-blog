3468c609226386f3465c076b714b85ed50c51221
jlonuz <lonuz.j@gmail.com>
2024-06-03 22:30:06 -0400

    add tailwind, shadcn and added services page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [preact()]
+  integrations: [preact(), tailwind({
+    applyBaseStyles: false
+  }), react()]
 });
